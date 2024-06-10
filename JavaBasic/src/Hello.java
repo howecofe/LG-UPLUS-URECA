d94f2bf7317cb4b677c39de53e8d70f4a6828a17
howecofe <am0229@naver.com>
2024-06-10 22:14:16 +0900

    ch01. 자바 시작하기

---
+++ JavaBasic/src/Hello.java
@@ -0,0 +1,43 @@
+
+
+public class Hello { // 클래스
+
+	public static void main(String[] args) { // 메소
+		System.out.println("Hello!");
+		
+	}
+
+}
+// 한 줄 주석
+
+/*
+여러줄 주석
+*/
+
+/**
+ * javadoc 명령어로 html 메뉴얼 문서를 만들 수 있는 주석
+ */
+
+/*
+java, C/C++, python, golang, javascript ...
+java : 백색가전제품
+java 가 브라우저에 탑재 -> 브라우저에서 java 실행 ==> 애플릿
+java 가 서버로 탑재 ==> 서블릿 jsp
+==> spring
+ */
+
+/*
+Virtual Machine
+
+.java                       .java
+.class -> 컴파일되면 생성됨     .class <- byte code, 이 파일을 실행
+------                      ------   
+JVM (Java Virtual Machine)   JVM
+--------------------------  -----
+Windows                     Linux
+ */
+
+/*
+
+
+ */
