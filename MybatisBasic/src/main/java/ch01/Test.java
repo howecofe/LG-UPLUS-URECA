b8386da82527db92ee9a68286ce25dc40be69abe
howecofe <am0229@naver.com>
2024-07-23 15:01:21 +0900

    DB: MyBatis ver.1

---
+++ MybatisBasic/src/main/java/ch01/Test.java
@@ -0,0 +1,69 @@
+package ch01;
+
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import ch01.dao.BookDao;
+import ch01.dto.BookDto;
+
+// version 1
+// config : xml
+// sql(mapper) : xml
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		// Mybatis 설정 파일을 읽어 온다.
+		Reader reader = Resources.getResourceAsReader("config/mybatis-config.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		SqlSession session = sqlSessionFactory.openSession();
+		
+		// SQL(mapper) + Java
+		// Java의 어떤 메소드(BookDao)가 호출되면 mapper의 어떤 sql이 수행되는지 연결
+		BookDao bookDao = session.getMapper(BookDao.class); // book-mapper.xml과 BookDao가 연결
+		
+		// 목록
+		{
+			List<BookDto> bookList = bookDao.listBook();
+			for (BookDto book : bookList) {
+				System.out.println(book);
+			}
+		}
+		
+		// 상세
+		{
+			BookDto bookDto = bookDao.detailBook(1);
+			System.out.println(bookDto);
+		}
+		
+		// 추가
+		{
+			BookDto bookDto = new BookDto(11, "11번째 책", "uplus press", 5000);
+			int ret = bookDao.insertBook(bookDto);
+			System.out.println(ret);
+			session.commit(); // insert, update, delete는 commit() 필요
+		}
+		
+		// 수정
+		{
+			BookDto bookDto = new BookDto(11, "11번째 책 수정", "uplus press 수정", 10000);
+			int ret = bookDao.updateBook(bookDto);
+			System.out.println(ret);
+			session.commit();
+		}
+		
+		// 삭제
+		{
+			int ret = bookDao.deleteBook(11);
+			System.out.println(ret);
+			session.commit();
+		}
+		
+		session.close();
+	}
+
+}
---
+++ MybatisBasic/src/main/java/ch01/dao/BookDao.java
@@ -0,0 +1,13 @@
+package ch01.dao;
+
+import java.util.List;
+
+import ch01.dto.BookDto;
+
+public interface BookDao {
+	List<BookDto> listBook();
+	BookDto detailBook(int bookId);
+	int insertBook(BookDto bookDto);
+	int updateBook(BookDto bookDto);
+	int deleteBook(int bookId);
+}
