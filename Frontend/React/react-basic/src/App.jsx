28c6f6105a23afc09311004a3169ea507bcd3f91
howecofe <am0229@naver.com>
2024-07-31 17:00:35 +0900

    FE: React - init setting

---
+++ Frontend/React/react-basic/src/App.jsx
@@ -0,0 +1,18 @@
+import ComponentTwoWay from "./views/ComponentTwoWay"
+import ComponentProps from "./views/ComponentProps"
+import LoginLogout from "./views/LoginLogout"
+import BoardView from "./views/BoardView"
+
+function App() {
+
+  return (
+    <>
+      {/* <ComponentTwoWay /> */}
+      {/* <ComponentProps /> */}
+      {/* <LoginLogout /> */}
+      <BoardView />
+    </>
+  )
+}
+
+export default App
